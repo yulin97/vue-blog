dba7775cfb06562d39132c8bb16079c22ba6df87
yulin <2240261428@qq.com>
2023-03-07 14:51:21 +0800

    初始化

---
+++ src/components/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>
+            First page
+        </h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "First page",
+
+}
+</script>
+
+<style></style>
